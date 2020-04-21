3624726c557152cde27b69ccdb5a846f6ff7db12
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-04-21 10:39:40 -0400

    :sparkles: Add turn.rb
    Add turn.rb

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+require './lib/card'
+
+class Turn
+  attr_reader :string, :card
+
+  def initialize(string, card)
+    @string = string
+    @card   = card
+  end
+
+  def guess
+    string
+  end
+
+  def correct?
+    string == card.answer
+  end
+
+  def feedback
+    return "Correct!" if correct?
+    return "Incorrect." if !correct?
+  end
+end
